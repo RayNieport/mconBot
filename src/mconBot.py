1252fbf34fd3699b9662dd3f0859cf516acdf03c
RayNieport <ray.nieport@gmail.com>
2021-12-09 15:13:29 -0500

    ports and >admin command

--- src/mconBot.py
+++ src/mconBot.py
@@ -1,24 +1,25 @@
 # mconBot.py
 # Ray Nieport 2021
 
-import os
-import json
 import discord
+from os import getenv
 from dotenv import load_dotenv
+from json import load
 from mcrcon import MCRcon
 
 # Get environment variables
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-USER_ROLE = os.getenv('DISCORD_USER_ROLE')
-MOD_ROLE = os.getenv('DISCORD_MOD_ROLE')
-ADMIN_ROLE = os.getenv('DISCORD_ADMIN_ROLE')
-IP = os.getenv('MINECRAFT_IP')
-PASS = os.getenv('MINECRAFT_PASS')
+TOKEN = getenv('DISCORD_TOKEN')
+USER_ROLE = getenv('DISCORD_USER_ROLE')
+MOD_ROLE = getenv('DISCORD_MOD_ROLE')
+ADMIN_ROLE = getenv('DISCORD_ADMIN_ROLE')
+IP = getenv('MINECRAFT_IP')
+PASS = getenv('MINECRAFT_PASS')
+PORT = int(getenv('RCON_PORT'))
 
 # Get dictionary of commands
 with open('commands.json') as cmd_file:
-    cmds = json.load(cmd_file)
+    cmds = load(cmd_file)
 
 # Create help message
 Help = discord.Embed(description="A bot to interact with your Minecraft server - from Discord!")
@@ -35,7 +36,7 @@ for com in cmds['admin_commands']:
 # Send command via rcon and print response
 async def send_rcon(cmd, args, message):
     try:
-        with MCRcon(IP, PASS) as mcr:
+        with MCRcon(IP, PASS, PORT) as mcr:
             if args:
                 resp = mcr.command(cmd + ' ' + args)
             else:
@@ -72,9 +73,17 @@ async def on_message(message):
     # handle command
     if cmd == 'help':
         await message.channel.send(embed=Help)
+        print (f'Helped out {message.author}.')
     elif cmd == 'hi':
         await message.channel.send('Hello! I\'m the Minecraft RCON bot!')
         print (f'Said hi to {message.author}.')
+    elif cmd == 'admin':
+        if authLevel >= 4:
+            cmd = args
+            args = ''
+            await send_rcon(cmd, args, message)
+        else:
+            await message.channel.send('Sorry, you need the ' + ADMIN_ROLE + ' role to use that command.')
     elif cmd in cmds['user_commands']:
         if authLevel >= 1:
             await send_rcon(cmd, args, message)
